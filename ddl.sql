11dccb82dbc92b7c2a2a300c7be197f11bfd7fd9
2228787638 <2228787638@qq.com>
2019-07-02 17:36:46 +0800

    finish ddl and dml

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,33 @@
--- **数据库级别：**  
+﻿-- **数据库级别：** 
+-- ddldb为数据库名
 --  显示所有数据库  
-
---  进入某个数据库  
-
+ show databases;
+--  进入某个数据库 
+ use ddldb;
 --  创建一个数据库  
-
+ create database ddldb;
 --  创建指定字符集的数据库  
-
+ create database ddldb default character set = 'utf8';
 --  显示数据库的创建信息   
-
+ show create database ddldb;
 --  修改数据库的编码  
-
+ alter database ddldb character set utf8;
 --  删除一个数据库   
-
+ drop database ddldb;
 -- **表级别**
 --  修改表名
-
+ rename table student to students;
 --  修改字段的数据类型
-
+ alter table student modify column age varchar(5);
 --  修改字段名
-
+ alter table student change name names varchar(40);
 --  添加字段
-
+ alter table student add hobby varchar(255);
 --  删除字段
-
+ alter table student drop hobby;
 --  修改表的存储引擎
-
---  删除表的外键约束
-
+ alter table student engine=myisam;
+--  删除表的外键约束(创建了一个user表，student表的user_id关联到user表的id)
+ alter table student drop foreign key user_id;
 --  删除一张表
+ drop table student;
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
--- 插入记录  
-
+﻿-- 插入记录 
+ insert into student (id,name,age,sex) values ("001","张三",18,"男");
 -- 修改记录  
-
+ update student set name="李四" where id="001";
 --  删除记录  
-
--- 查询记录  
+ delete from student where id="001";
+-- 查询记录 
+ select * from student;
